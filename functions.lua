0d8d52e18f1ae5ea301b8adaf09ef8c8a34e019e
Marshy <84200284+Marshxan@users.noreply.github.com>
2022-10-22 23:12:10 +0100

    Create functions.lua

---
+++ functions.lua
@@ -0,0 +1,15 @@
+function QBCore.Functions.Notify(text, textype, length)
+    if textype == "primary" then textype = "inform" end
+    if type(text) == "table" then
+        local ttext = text.text or 'Placeholder'
+        local caption = text.caption or 'Placeholder'
+        local ttype = textype or 'inform'
+        local length = length or 3000
+        exports['okokNotify']:Alert("🔔   Notification   🔔", text, length, "info")
+    else
+        local ttype = textype or 'inform'
+        local ttext = text.text or 'Placeholder'
+        local length = length or 5000
+        exports['okokNotify']:Alert("🔔   Notification   🔔", text, length, "info")
+    end
+end
